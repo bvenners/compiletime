4cd7f22f4a5d799e81b3008bc040baa68f9354f0
bvenners <bill@artima.com>
2013-02-09 14:46:21 -0500

    Tweaked.

--- runit.sh
+++ runit.sh
@@ -1,6 +1,7 @@
 JAVA_OPTS="-server -Xmx2048M -Xms256M"
 export JAVA_OPTS
-scala allMethodTestsInOneFile.scala
+scala testsIn100Files.scala
+# scala allMethodTestsInOneFile.scala
 # scala allClassTestsInOneFile.scala
 # scala allTestsInOneFile.scala
 # scala dataTables.scala
