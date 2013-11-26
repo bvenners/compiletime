643f6cc94423e4c4201f755b98af9fa0263eb766
cheeseng <cheeseng@amaseng.com>
2013-11-26 12:00:59 +0800

    Fixed a bug in shapeless table implementation, and added a TableChecker that provides a forAll assertion method.

--- ShapelessTable.scala
+++ ShapelessTable.scala
@@ -1,27 +1,41 @@
-package shapelessTable
+package shapelessTable {
 
 import shapeless._
 
-class Table[TH, TR](hdrs: TH, rows: TR*)
+  class Table[TH, TR](val hdrs: TH, val rows: TR*)
 
-object Table {
-  def apply[TH, TR](hdrs: TH, rows: TR*)
-                   (implicit ts: TableShape[TH, TR]) = new Table(hdrs, rows)
+  object Table {
+    def apply[TH, TR](hdrs: TH, rows: TR*)
+                     (implicit ts: TableShape[TH, TR]) = new Table(hdrs, rows: _*)
 
-  trait TableShape[TH, TR]
+    trait TableShape[TH, TR]
 
-  object TableShape {
-    implicit def productTableShape[TH, TR, LH, LR]
-    (implicit
-     genH: Generic.Aux[TH, LH],
-     genR: Generic.Aux[TR, LR],
-     hlistShape: TableShape[LH, LR]): TableShape[TH, TR] = new TableShape[TH, TR] {}
+    object TableShape {
+      implicit def productTableShape[TH, TR, LH, LR]
+      (implicit
+       genH: Generic.Aux[TH, LH],
+       genR: Generic.Aux[TR, LR],
+       hlistShape: TableShape[LH, LR]): TableShape[TH, TR] = new TableShape[TH, TR] {}
 
-    implicit def hsingleTableShape[RH]: TableShape[String :: HNil, RH :: HNil] =
-      new TableShape[String :: HNil, RH :: HNil] {}
+      implicit def hsingleTableShape[RH]: TableShape[String :: HNil, RH :: HNil] =
+        new TableShape[String :: HNil, RH :: HNil] {}
 
-    implicit def hlistTableShape[HT <: HList, RH, RT <: HList]
-    (implicit tailShape: TableShape[HT, RT]): TableShape[String :: HT, RH :: RT] =
-      new TableShape[String :: HT, RH :: RT] {}
+      implicit def hlistTableShape[HT <: HList, RH, RT <: HList]
+      (implicit tailShape: TableShape[HT, RT]): TableShape[String :: HT, RH :: RT] =
+        new TableShape[String :: HT, RH :: RT] {}
+    }
   }
+}
+
+package shapeless {
+
+  trait TableChecker {
+    def forAll[TH, TR](table: shapelessTable.Table[TH, TR])(fun: TR => Unit) {
+      table.rows.foreach { row =>
+        fun(row)
+      }
+    }
+  }
+
+  object TableChecker extends TableChecker
 }
